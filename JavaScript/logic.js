000b770c0c97f9b794c250fb3346f21d7b7de6fd
Giorgi Kintsurashvili <giokintsurashvili0@gmail.com>
2022-12-11 20:26:13 +0400

    added product creation feature

--- JavaScript/logic.js
+++ JavaScript/logic.js
@@ -1,48 +1,45 @@
 //creating default products
 const tBody=document.getElementById("rows-container");
 const productsNumber = 10;
-var colorArray = ['Red','Black'];
-var idArray = [92, 38];
-var precision = 100; // 2 decimals
-var randomNum = Math.floor(Math.random() * (10 * precision - 1 * precision) + 1 * precision) / (1*precision);
+var totalProducts=[];
+const colorArray = ['Red','Black'];
+const idArray = [92, 38];
+const precision = 100; // 2 decimals
 function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toISOString().slice(0,10);
 }
+  
 
+function defaultProducts(c){
+    const tableRow = document.createElement("tr");
+    const tableHead = document.createElement("th");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    const td3 = document.createElement("td");
+    const td4 = document.createElement("td");
+    const td5 = document.createElement("td");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
 
-
-for(let i=0; i<productsNumber;i++){
-    var tableRow = document.createElement("tr");
-    var tableHead = document.createElement("th");
-    var td1 = document.createElement("td");
-    var td2 = document.createElement("td");
-    var td3 = document.createElement("td");
-    var td4 = document.createElement("td");
-    var td5 = document.createElement("td");
-    var editButton = document.createElement("button");
-    var deleteButton = document.createElement("button");
-
-    var randomString = Math.floor(Math.random()*colorArray.length);
-    var randomNumber = Math.floor(Math.random()* (60 - 40 + 1)) + 40;
-    var randomID = Math.floor(Math.random()*idArray.length);
-    var randomNum = Math.floor(Math.random() * (1500 * precision - 330 * precision) + 330 * precision) / (1*precision);
+    const randomString = Math.floor(Math.random()*colorArray.length);
+    const randomNumber = Math.floor(Math.random()* (60 - 40 + 1)) + 40;
+    const randomID = Math.floor(Math.random()*idArray.length);
+    const randomNum = Math.floor(Math.random() * (1500 * precision - 330 * precision) + 330 * precision) / (1*precision);
     
-    tableHead.setAttribute("scope",`${i+1}`);
+    tableHead.setAttribute("scope",`${c+1}`);
+    totalProducts.push(c);
     editButton.setAttribute("type", "button");
-    //editButton.setAttribute("data-bs-toggle", "modal");
-    //editButton.setAttribute("data-bs-target","#exampleModal")
     deleteButton.setAttribute("type", "button");
-    //deleteButton.setAttribute("data-bs-toggle", "modal");
-    //deleteButton.setAttribute("data-bs-target","#exampleModal")
     editButton.classList.add("btn","btn-secondary");
     deleteButton.classList.add("btn","btn-danger");
     deleteButton.style.marginLeft="5%";
-
+    editButton.setAttribute("data-bs-toggle", "modal");
+    editButton.setAttribute("data-bs-target","#CreateModal") 
     tableHead.textContent += `HL Road Frame - ${colorArray[randomString]}, ${randomNumber}`;
     td1.textContent += `FR-R${idArray[randomID]}${colorArray[randomString].substring(0, 1)}-${randomNumber}`;
     td2.textContent += `${colorArray[randomString]}`;
     td3.textContent += `${randomNum}`;
-    td4.textContent += `${randomDate(new Date(2012, 0, 1), new Date())}`; 
+    td4.textContent += `${randomDate(new Date(1, 0, 2012), new Date())}`; 
     editButton.textContent += "Edit";
     deleteButton.textContent += "Delete";
 
@@ -51,10 +48,73 @@ for(let i=0; i<productsNumber;i++){
     document.getElementById('rows-container').appendChild(tableRow)
 }
 
+for(let i=0; i<productsNumber;i++){
+    defaultProducts(i);
+}
+//----------------------------------------------------------------------------------
+function closeProduct(){
 
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+  document.getElementById("inputName").value='';
+    document.getElementById("inputNumber").value='';
+    document.getElementById("inputColor").value='';
+    document.getElementById("inputCost").value='';
+    document.getElementById("inputPrice").value='';
+    document.getElementById("inputSize").value='';
+    document.getElementById("inputWeight").value='';
+    document.getElementById("inputDate").value='';
+}
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+function setEditProduct(clickedId){
+  if(clickedId == "addProduct" ){
+    var inputName = document.getElementById("inputName").value;
+    var inputNumber = document.getElementById("inputNumber").value;
+    var inputColor = document.getElementById("inputColor").value;
+    var inputCost = document.getElementById("inputCost").value;
+    var inputPrice = document.getElementById("inputPrice").value;
+    var inputSize = document.getElementById("inputSize").value;
+    var inputWeight = document.getElementById("inputWeight").value;
+    var inputDate = document.getElementById("inputDate").value;
+    if(inputName!='' && inputNumber!=''){
+     
+      const tableRow = document.createElement("tr");
+      const tableHead = document.createElement("th");
+      const td1 = document.createElement("td");
+      const td2 = document.createElement("td");
+      const td3 = document.createElement("td");
+      const td4 = document.createElement("td");
+      const td5 = document.createElement("td");
+      const editButton = document.createElement("button");
+      const deleteButton = document.createElement("button");
+  
+      tableHead.setAttribute("scope",`${totalProducts.length+1}`);
+      totalProducts.push(totalProducts.length);
+      editButton.setAttribute("type", "button");
+      deleteButton.setAttribute("type", "button");
+      editButton.classList.add("btn","btn-secondary");
+      deleteButton.classList.add("btn","btn-danger");
+      deleteButton.style.marginLeft="5%";
+  
+      tableHead.textContent += `${inputName}`;
+      td1.textContent += `${inputNumber}`;
+      td2.textContent += `${inputColor}`;
+      td3.textContent += `${inputPrice}`;
+      td4.textContent += `${inputDate}`; 
+      editButton.textContent += "Edit";
+      deleteButton.textContent += "Delete";
+  
+      td5.append(editButton,deleteButton);
+      tableRow.append(tableHead,td1,td2,td3,td4,td5);
+      document.getElementById('rows-container').appendChild(tableRow)
+    }
+      document.getElementById("inputName").value='';
+      document.getElementById("inputNumber").value='';
+      document.getElementById("inputColor").value='';
+      document.getElementById("inputCost").value='';
+      document.getElementById("inputPrice").value='';
+      document.getElementById("inputSize").value='';
+      document.getElementById("inputWeight").value='';
+      document.getElementById("inputDate").value='';
+  }else{
+//sue scope
+  }
+}
